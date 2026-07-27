481f1c1b905e4fe6e65f534e33793cfef9b9103b
ADVANGithub <159441771+ADVANGithub@users.noreply.github.com>
2026-07-28 01:29:23 +0530

    Update get-course.js

--- api/get-course.js
+++ api/get-course.js
@@ -1,1 +1,47 @@
+import jwt from "jsonwebtoken";
 
+export default async function handler(req,res){
+
+try{
+
+const auth=req.headers.authorization;
+
+if(!auth){
+
+return res.status(401).json({
+
+success:false
+
+});
+
+}
+
+const token=auth.replace("Bearer ","");
+
+jwt.verify(
+
+token,
+
+process.env.JWT_SECRET
+
+);
+
+return res.status(200).json({
+
+success:true,
+
+course:"Master AI From Scratch"
+
+});
+
+}catch{
+
+return res.status(401).json({
+
+success:false
+
+});
+
+}
+
+}
